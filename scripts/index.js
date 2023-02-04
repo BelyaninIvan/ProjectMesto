a1446c096903b986fa693a310ad315c10296ffc5
Ivan Belyanin <belyanin-2001@list.ru>
2023-02-05 00:04:37 +0400

    Sprint 6; First part

--- scripts/index.js
+++ scripts/index.js
@@ -133,3 +133,54 @@ const renderCard = (data, wrap) => {
 initialCards.forEach((data) => {
     renderCard(data, cardList);
 });
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    const submitButton = formElement.querySelector('.popup__submit-button');
+    inputElement.classList.add('popup__input_type_error');
+    submitButton.classList.add('popup__submit-button_inactive');
+    submitButton.setAttribute('disable', true);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('popup__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    const submitButton = formElement.querySelector('.popup__submit-button');
+    inputElement.classList.remove('popup__input_type_error');
+    submitButton.classList.remove('popup__submit-button_inactive');
+    submitButton.removeAttribute('disable', false);
+    errorElement.classList.remove('popup__input-error_active');
+    errorElement.textContent = '';
+}
+
+const isValid = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+};
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement);
+        });
+    });
+};
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', function(evt) {
+            evt.preventDefault();
+          });
+        setEventListeners(formElement);
+    });
+};
+
+enableValidation();
